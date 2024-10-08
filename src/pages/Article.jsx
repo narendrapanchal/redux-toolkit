e40b388fe6d3459e7d98742df93504c8b97b9d43
narendrapanchal <narendrapanchal020@gmail.com>
2024-10-08 22:14:24 +0530

    blogs-added

---
+++ src/pages/Article.jsx
@@ -0,0 +1,25 @@
+import  { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchData } from "../routes/AllRoutes";
+
+function Article() {
+  const { id } = useParams();
+  const [data, setData] = useState();
+  useEffect(() => {
+    (async () => {
+      const response = await fetchData(
+        `https://jsonplaceholder.typicode.com/posts/${id}`
+      );
+      setData(response);
+    })();
+  }, [id]);
+
+  return (
+    <div className="shadow-lg my-4 p-2  rounded">
+      <h1 className="font-bold text-3xl text-left my-2">{data?.title}</h1>
+      <p className="text-left mb-2">{data?.body}</p>
+    </div>
+  );
+}
+
+export default Article;
--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,18 +1,32 @@
 import ProductList from '../pages/ProductList';
 import Product from '../pages/Product';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import HomeRoute from '../pages/HomeRoute';
 import Home from '../pages/Home';
 import ProductWrapper from '../pages/ProductWrapper';
 
 
-const fetchData= async(url)=>{
+export const fetchData= async(url)=>{
     console.log(url)
     const response=await fetch(url);
     return await response.json();
 }
 import Blogs from '../pages/Blogs';
 import ContactUs from '../pages/ContactUs';
+import Article from '../pages/Article';
+export const fetchPosts = async (limit = 10, page = 1) => {
+    try {
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data = await response.json();
+      
+      return {
+        data: data.slice((page - 1) * limit, (page - 1) * limit + limit),
+        pages: Math.ceil(data.length / limit),
+      };
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
+  };
 
 const router = createBrowserRouter([
     {
@@ -41,7 +55,20 @@ const router = createBrowserRouter([
             },
             {
                 path: "/blogs",
-                element: <Blogs />,
+                element: <Outlet />,
+                children:[
+                    {
+                        path:":id",
+                        element:<Blogs />,
+                        loader:()=>fetchPosts()
+                    }
+                    ,
+                    {
+                        path:'article/:id',
+                        element:<Article/>,
+                        loader:({params})=>fetchData(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+                    }
+                ]
             },
             {
                 path: "/contact",
