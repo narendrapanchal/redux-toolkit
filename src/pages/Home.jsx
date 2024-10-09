7070cc7b74ed9e11aa35d00cef521ee6a2e2feb6
Ankit Kaushik <ankitkaushikdhn@gmail.com>
2024-10-09 10:53:41 +0530

    UI updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { useLoaderData } from 'react-router-dom'
 import ProductCard from '../components/ProductCard'
+import Slider from '../components/Slider'
 
 const Home = () => {
-  const data=useLoaderData()
-   
+  const data = useLoaderData()
+
   return (
-    <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-x-6 gap-y-10 container'>
-      {data.map((product,index)=><ProductCard {...product} key={product.id}/>)}
+    <div>
+      <Slider />
+      <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-x-6 gap-y-10 container'>
+        {data.map((product, index) => <ProductCard {...product} key={product.id} />)}
+      </div>
     </div>
   )
 }
